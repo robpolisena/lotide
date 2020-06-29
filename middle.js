f4aaefad65aa89b8f6c7f3765379c4220e11ea08
Rob <robpolisena@gmail.com>
2020-06-29 16:53:45 +0000

    refactored to contain function and export only

--- middle.js
+++ middle.js
@@ -1,29 +1,4 @@
-const eqArrays = function(array1, array2) {
-  let result = 0;
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      result += 1;
-    } else {
-      result += 0;
-    }
-  }
-  if (result === 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2) === true) {
-    console.log("Assertion Passed: The arrays are equal");
-  } else {
-    console.log("Assertion Failed: The arrays are not equal");
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(array) {
   let newArray = [];
@@ -44,11 +19,4 @@ const middle = function(array) {
   }
 };
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+module.exports = middle;
