561d7a7c5020cca10fa4b6148585a798135cb9c3
Rob <robpolisena@gmail.com>
2020-06-29 11:46:33 +0000

    test for assertequal.js

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+// test/assertEqualTest.js
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+
+module.exports = assertEqual;
