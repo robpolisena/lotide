a191cad6d0ac350c6d798ded9e0d070c3ba71d10
Rob <robpolisena@gmail.com>
2020-06-29 18:55:22 +0000

    added mocha and chai to our test code

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,11 @@
 // test middle.js
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-// TEST CODE
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+describe("#middle", () => {
+  it("returns the middle element of an array", () => {
+    assert.deepEqual(middle([1]), []);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
