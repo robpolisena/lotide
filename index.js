b9daf44cc4bbd2801e406880e4450e8415189f8c
Rob <robpolisena@gmail.com>
2020-06-29 22:12:20 +0000

    contains object of all utility functions of lotide project

---
+++ index.js
@@ -0,0 +1,36 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require("./eqArrays");
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const eqObjects = require('./eqObjects');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  eqArrays: eqArrays,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  eqObjects: eqObjects,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without,
+};
